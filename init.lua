eb13fee611cdbe8a81fdd24fad9994f807a685ae
Phandal <bfeld.111@gmail.com>
2021-12-12 12:10:05 -0600

    Added support for cmp instead of compe and added vnsip integration

--- init.lua
+++ init.lua
@@ -48,7 +48,14 @@ require 'paq-nvim' {
 	'norcalli/nvim-base16.lua';
 	'neovim/nvim-lspconfig';
 	'hoob3rt/lualine.nvim';
-  'hrsh7th/nvim-compe';
+  --'hrsh7th/nvim-compe';
+  'hrsh7th/cmp-nvim-lsp';
+  'hrsh7th/cmp-buffer';
+  'hrsh7th/cmp-path';
+  'hrsh7th/cmp-cmdline';
+  'hrsh7th/nvim-cmp';
+  'hrsh7th/cmp-vsnip';
+  'hrsh7th/vim-vsnip';
   'lukas-reineke/indent-blankline.nvim';
   'windwp/nvim-autopairs';
   'kyazdani42/nvim-web-devicons';
@@ -105,6 +112,133 @@ vim.api.nvim_command([[autocmd BufWinLeave agenda.md mkview]])
 vim.g.neovide_cursor_vfx_mode = "pixiedust"
 Opt("o", "guifont", "NotoMono NF,all-the-icons")
 
+-- CMP setup
+local cmp = require'cmp'
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete,
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    -- Accept currently selected item. If none selected, `select` first item.
+    -- Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    { name = 'buffer' },
+    { name = 'path' },
+  })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- Vsnip setup
+keys("i", "<Tab>", [[vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>']], {expr = true})
+keys("i", "<S-Tab>", [[vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>']], {expr = true})
+keys("s", "<Tab>", [[vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>']], {expr = true})
+keys("s", "<S-Tab>", [[vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>']], {expr = true})
+---- ### START OF COMPE
+---- -- Compe Setup
+---- require'compe'.setup {
+----   enabled = true;
+----   autocomplete = true;
+----   debug = false;
+----   min_length = 1;
+----   preselect = 'enable';
+----   throttle_time = 80;
+----   source_timeout = 200;
+----   incomplete_delay = 400;
+----   max_abbr_width = 100;
+----   max_kind_width = 100;
+----   max_menu_width = 100;
+----   documentation = true;
+---- 
+----   source = {
+----     path = true;
+----     nvim_lsp = true;
+----   };
+---- }
+---- 
+---- local t = function(str)
+----   return vim.api.nvim_replace_termcodes(str, true, true, true)
+---- end
+---- 
+---- local check_back_space = function()
+----     local col = vim.fn.col('.') - 1
+----     if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+----         return true
+----     else
+----         return false
+----     end
+---- end
+---- 
+---- -- Use (s-)tab to:
+---- --- move to prev/next item in completion menuone
+---- --- jump to prev/next snippet's placeholder
+---- _G.tab_complete = function()
+----   if vim.fn.pumvisible() == 1 then
+----     return t "<C-n>"
+----   elseif check_back_space() then
+----     return t "<Tab>"
+----   else
+----     return vim.fn['compe#complete']()
+----   end
+---- end
+---- _G.s_tab_complete = function()
+----   if vim.fn.pumvisible() == 1 then
+----     return t "<C-p>"
+----   else
+----     return t "<S-Tab>"
+----   end
+---- end
+---- 
+---- --vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+---- --vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+---- --vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+---- --vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+---- 
+---- function _G.completions()
+----     local npairs = require("nvim-autopairs")
+----     if vim.fn.pumvisible() == 1 then
+----         if vim.fn.complete_info()["selected"] ~= -1 then
+----             return vim.fn["compe#confirm"]("<CR>")
+----         end
+----     end
+----     return npairs.check_break_line_char()
+---- end
+---- 
+---- vim.api.nvim_set_keymap("i", "<CR>", "v:lua.completions()", {expr = true})
+----
+---- #### END OF COMPE
+
+
 -- Lsp Config
 local nvim_lsp = require('lspconfig')
 
@@ -136,11 +270,14 @@ local on_attach = function(client, bufnr)
 
 end
 
+-- Capabilities for nvim-cmp
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local servers = { "pyright", "rust_analyzer", "ccls" }
 for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup { on_attach = on_attach }
+  nvim_lsp[lsp].setup { on_attach = on_attach, capabilities = capabilities }
 end
 
 -- My Rust-tools setup
@@ -189,77 +326,6 @@ vim.highlight.create('LineNr', {guibg='#2E3440'}, false)
 vim.highlight.create('SignColumn', {guibg='#2E3440'}, false)
 
 
--- Compe Setup
-require'compe'.setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 80;
-  source_timeout = 200;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = true;
-
-  source = {
-    path = true;
-    nvim_lsp = true;
-  };
-}
-
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
-end
-
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-    return vim.fn['compe#complete']()
-  end
-end
-_G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  else
-    return t "<S-Tab>"
-  end
-end
-
---vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
---vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
---vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
---vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-
-function _G.completions()
-    local npairs = require("nvim-autopairs")
-    if vim.fn.pumvisible() == 1 then
-        if vim.fn.complete_info()["selected"] ~= -1 then
-            return vim.fn["compe#confirm"]("<CR>")
-        end
-    end
-    return npairs.check_break_line_char()
-end
-
-vim.api.nvim_set_keymap("i", "<CR>", "v:lua.completions()", {expr = true})
-
 -- Devicons setup
 require 'nvim-web-devicons'.setup{default = true;}
 
@@ -282,7 +348,7 @@ require 'nvim-treesitter.configs'.setup {
 };
 
 -- NvimTree setup
---vim.g.nvim_tree_auto_close = 1
+require 'nvim-tree'.setup()
 
 -- Telescope Setup
 local telescope_actions = require('telescope.actions')
