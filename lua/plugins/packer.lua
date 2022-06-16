54890a9a39018f9375e422f430ea581c208d2c45
Bailey <bfeld.111@gmail.com>
2022-06-16 00:15:14 -0500

    Added an install script for packer

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -1,17 +1,30 @@
-require('packer').startup(function()
-  use 'wbthomason/packer.nvim'                                  -- Plugin Manager
-  use 'norcalli/nvim-base16.lua'                                -- Theme
-  use { 'kyazdani42/nvim-tree.lua',                             -- Tree like file manager
+local fn = vim.fn
+local packer_install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(packer_install_path)) > 0 then
+	packer_bootstrap = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		packer_install_path,
+	})
+end
+
+require('packer').startup(function(use)
+  use ('wbthomason/packer.nvim')                                  -- Plugin Manager
+  use ('norcalli/nvim-base16.lua')                                -- Theme
+  use ({ 'kyazdani42/nvim-tree.lua',                              -- Tree like file manager
         requires = { 'kyazdani42/nvim-web-devicons' }           
-      }
-  use 'hoob3rt/lualine.nvim'                                    -- Different modeline
-  use { 'nvim-telescope/telescope.nvim',                          -- Fuzzy Finder
+      })
+  use ('hoob3rt/lualine.nvim')                                    -- Different modeline
+  use ({ 'nvim-telescope/telescope.nvim',                          -- Fuzzy Finder
         requires = { 'nvim-lua/plenary.nvim' }
-      }
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }  -- Syntax highlighting using TreeSitter
-  use 'windwp/nvim-autopairs'                                   -- Automatically adds closing bracket
-  use 'lukas-reineke/indent-blankline.nvim'                     -- Shows a symbol for indents
-  use 'neovim/nvim-lspconfig'                                   -- Base set of LSP configs
+      })
+  use ({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })  -- Syntax highlighting using TreeSitter
+  use ('windwp/nvim-autopairs')                                   -- Automatically adds closing bracket
+  use ('lukas-reineke/indent-blankline.nvim')                     -- Shows a symbol for indents
+  use ('neovim/nvim-lspconfig')                                   -- Base set of LSP configs
 
   -- use 'hrsh7th/nvim-cmp'                                        -- Completion engine
   -- use 'hrsh7th/cmp-nvim-lsp'                                    -- Completion for LSP
@@ -22,4 +35,7 @@ require('packer').startup(function()
   -- use 'hrsh7th/vim-vsnip'                                       -- Snippets plugin
   -- use 'nvim-lua/popup.nvim'                                     -- Dependency Plugin
   -- use 'nvim-lua/plenary.nvim'                                   -- Dependency Plugin
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)
