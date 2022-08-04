7008291573f5262f30255886ff130c7060301476
Phandal <bfeld.111@gmail.com>
2022-08-04 12:41:15 -0500

    added snippets

--- lua/plugins/config/luasnip.lua
+++ lua/plugins/config/luasnip.lua
@@ -1,1 +1,2 @@
-require("luasnip.loaders.from_vscode").lazy_load({paths = "~/.config/snippets/netsuite.json"})
+-- require("luasnip.loaders.from_vscode").lazy_load({paths = "~/.config/snippets/netsuite.json"})
+require("luasnip.loaders.from_vscode").lazy_load({paths = "~/Appdata/Local/nvim/snippets"})
