0c21a2293c5914c02661ce607761e308749f127a
Phandal <bfeld.111@gmail.com>
2022-08-27 08:37:04 -0500

    updated the look of statusline

--- lua/plugins/config/lualine.lua
+++ lua/plugins/config/lualine.lua
@@ -9,8 +9,10 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = theme,
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    -- component_separators = { left = '', right = ''},
+    -- section_separators = { left = '', right = ''},
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
     disabled_filetypes = {},
     always_divide_middle = true,
     globalstatus = false,
