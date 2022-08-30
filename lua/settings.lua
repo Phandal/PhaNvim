37d503a3f73fcd58d1d14fae78f43e63ad91f7e5
Phandal <bfeld.111@gmail.com>
2022-08-30 08:07:09 -0500

    Added quickfix autocmd

--- lua/settings.lua
+++ lua/settings.lua
@@ -39,6 +39,7 @@ autocmd("TermOpen", { pattern = "*", command = "lua vim.opt.number = false" })
 autocmd("TermOpen", { pattern = "*", command = "lua vim.opt.signcolumn = 'no'" })
 autocmd("TermOpen", { pattern = "*", command = "startinsert" })
 autocmd("FileType", { pattern = "help", command = "nnoremap q <CMD>bd!<CR>" })
+autocmd("FileType", { pattern = "qf", command = "nnoremap q <CMD>bd!<CR>" })
 autocmd("BufWritePre", { pattern = "*.tsx,*.ts,*.jsx,*.js", command = "EslintFixAll" })
 
 -- Highlights
