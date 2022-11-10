7e9af6c70673fe3d41d190ebec25505cb1f726c5
Bailey <bailey.feldewerd@bergankdv.com>
2022-11-09 18:16:20 -0600

    updated to use the dev and prod setup for sdfcli-nvim

--- lua/plugins/config/sdfcli.lua
+++ lua/plugins/config/sdfcli.lua
@@ -1,11 +1,13 @@
 local cmd_location = nil
-if vim.loop.os_uname().sysname == 'Darwin' then
+local sysname = vim.loop.os_uname().sysname
+if sysname == 'Darwin' or sysname == 'Linux' then
   cmd_location = vim.env.HOME .. '/.local/bin/sdfcli'
 else
   cmd_location = '/sdfcli/sdfcli.bat'
 end
 
--- Setup for the plugin
+-- Setup for the plugin (Still under development)
 require('sdfcli-nvim').setup({
-  sdfcli_cmd_path = cmd_location
+  sdfcli_cmd_path = cmd_location,
+  sdfcli_cmd = cmd_location,
 })
