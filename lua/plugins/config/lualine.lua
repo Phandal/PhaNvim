459c32ba61ffeef6b1d4eaf42272c37394b85891
Phandal <bfeld.111@gmail.com>
2022-11-25 15:41:07 -0600

    added bufferline.nvim

--- lua/plugins/config/lualine.lua
+++ lua/plugins/config/lualine.lua
@@ -47,21 +47,21 @@ require('lualine').setup {
     lualine_z = {}
   },
   tabline = {},
-  winbar = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  inactive_winbar = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {}
-  },
+  -- winbar = {
+  --   lualine_a = {},
+  --   lualine_b = {},
+  --   lualine_c = {'filename'},
+  --   lualine_x = {},
+  --   lualine_y = {},
+  --   lualine_z = {}
+  -- },
+  -- inactive_winbar = {
+  --   lualine_a = {},
+  --   lualine_b = {},
+  --   lualine_c = {'filename'},
+  --   lualine_x = {},
+  --   lualine_y = {},
+  --   lualine_z = {}
+  -- },
   extensions = {'nvim-tree', 'quickfix'}
 }
