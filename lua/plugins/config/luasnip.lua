b5ae92287b5976a1ac4a9d7f0024f22eb580f5e0
Bailey <bailey.feldewerd@bergankdv.com>
2022-12-23 11:34:50 -0600

    updated blankline to not show the trailing indent

--- lua/plugins/config/luasnip.lua
+++ lua/plugins/config/luasnip.lua
@@ -1,2 +1,2 @@
--- require("luasnip.loaders.from_vscode").lazy_load({paths = "~/.config/snippets/netsuite.json"})
+require("luasnip.loaders.from_vscode").lazy_load({paths = "~/.config/nvim/snippets"})
 require("luasnip.loaders.from_vscode").lazy_load({paths = "~/Appdata/Local/nvim/snippets"})
