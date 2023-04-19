ee1f82fe75f3dede07a13af2a1bb993329b74288
Phandal <bfeld.111@gmail.com>
2023-04-19 00:09:24 -0500

    cleanup

--- lua/settings.lua
+++ lua/settings.lua
@@ -17,7 +17,6 @@ opt.splitright = true
 opt.splitbelow = true
 opt.hidden = true
 opt.backup = false
-opt.writebackup = false
 opt.updatetime = 300
 opt.shortmess = "filnxtToOFc"
 opt.signcolumn = "yes"
@@ -29,10 +28,8 @@ opt.laststatus = 3
 opt.fillchars = "eob: ,vert:│"
 opt.cursorline = true
 opt.list = true
--- opt.listchars = opt.listchars + 'eol:﬋, trail:∙'
--- opt.listchars = opt.listchars + 'trail:∙'
+opt.listchars = opt.listchars + 'eol:﬋, trail:∙'
 opt.relativenumber = true
--- opt.virtualedit = "all"
 opt.guicursor = "n-v-c-sm:block-blinkwait300-blinkon200-blinkoff150,i-ci-ve:ver25-blinkwait300-blinkon200-blinkoff150,r-cr-o:hor20"
 
 -- Global variables
