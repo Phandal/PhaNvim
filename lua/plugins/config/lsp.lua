7fee066a10eeed173efae9ccdc8d2f589b23cc00
Phandal <bfeld.111@gmail.com>
2023-05-02 22:00:21 -0500

    updated lsp signcolumn

--- lua/plugins/config/lsp.lua
+++ lua/plugins/config/lsp.lua
@@ -49,7 +49,7 @@ local on_attach = function(client, bufnr)
   end
 end
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
