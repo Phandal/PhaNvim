e740f8365197246e1233d8195d480070e358b59a
Phandal <bfeld.111@gmail.com>
2023-05-30 23:01:27 -0500

    updated colors to work with Telescope

--- lua/plugins/config/colors.lua
+++ lua/plugins/config/colors.lua
@@ -1,9 +1,9 @@
--- vim.g.nord_contrast = true
+vim.g.nord_contrast = true
 vim.g.nord_borders = true
-vim.g.nord_disable_background = true
--- vim.g.nord_cursorline_transparent = false
+vim.g.nord_disable_background = false
+vim.g.nord_cursorline_transparent = false
 vim.g.nord_enable_sidebar_background = true
--- vim.g.nord_italic = true
--- vim.g.nord_uniform_diff_background = false
--- vim.g.nord_bold = true
+vim.g.nord_italic = true
+vim.g.nord_uniform_diff_background = false
+vim.g.nord_bold = true
 require('nord').set()
