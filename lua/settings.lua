25701a403781cbee7d45661299833dbe415e6132
Phandal <bfeld.111@gmail.com>
2023-07-01 20:00:01 -0500

    shiftround for better indentation

--- lua/settings.lua
+++ lua/settings.lua
@@ -31,6 +31,7 @@ opt.list = true
 opt.listchars = opt.listchars + 'eol:󱞥, trail:·'
 opt.relativenumber = true
 opt.guicursor = "n-v-c-sm:block-blinkwait300-blinkon200-blinkoff150,i-ci-ve:ver25-blinkwait300-blinkon200-blinkoff150,r-cr-o:hor20"
+opt.shiftround = true
 
 -- Global variables
 g.mapleader = " "
