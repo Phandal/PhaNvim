9e470f2042fc220a6c1d786ca5c1ba15bc15c51b
Phandal <bfeld.111@gmail.com>
2023-08-21 11:08:16 -0500

    added injections for embedded languages in typescript

---
+++ after/queries/typescript/injections.scm
@@ -0,0 +1,12 @@
+; extends
+(lexical_declaration
+  (variable_declarator 
+    (comment) @comment (#eq? @comment "/*sql*/")
+    (template_string) @injection.content
+    (#set! injection.language "sql")))
+
+(lexical_declaration
+  (variable_declarator 
+    (comment) @comment (#eq? @comment "/*html*/")
+    (template_string) @injection.content
+    (#set! injection.language "html")))
