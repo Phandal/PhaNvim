62eb1e2d84b005f098d019e5e394b5a7a16643b0
Phandal <bfeld.111@gmail.com>
2023-10-13 21:12:54 -0500

    acatually for real fixed the injections

--- after/queries/typescript/injections.scm
+++ after/queries/typescript/injections.scm
@@ -2,9 +2,9 @@
 (lexical_declaration
   (variable_declarator 
     (comment) @comment (#eq? @comment "/*sql*/")
-    (template_string) @injection.content (#offset! @injection.content 1 0 0 0)))
+    (template_string) @injection.content (#set! injection.language "sql")))
 
 (lexical_declaration
   (variable_declarator 
     (comment) @comment (#eq? @comment "/*html*/")
-    (template_string) @injection.content (#offset! @injection.content 1 0 0 0)))
+    (template_string) @injection.content (#set! injection.language "html")))
