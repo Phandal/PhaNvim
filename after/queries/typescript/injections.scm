eb3d5265ce72a5ecb39b72fa69dbbfa6b7948066
Phandal <bfeld.111@gmail.com>
2024-03-11 11:21:10 -0500

    updated TS injections for return statements

--- after/queries/typescript/injections.scm
+++ after/queries/typescript/injections.scm
@@ -4,7 +4,15 @@
     (comment) @comment (#eq? @comment "/*sql*/")
     (template_string) @injection.content (#set! injection.language "sql")))
 
+(return_statement
+  (comment) @comment (#eq? @comment "/*sql*/")
+  (template_string) @injection.content (#set! injection.language "sql"))
+
 (lexical_declaration
   (variable_declarator 
     (comment) @comment (#eq? @comment "/*html*/")
     (template_string) @injection.content (#set! injection.language "html")))
+
+(return_statement
+  (comment) @comment (#eq? @comment "/*html*/")
+  (template_string) @injection.content (#set! injection.language "html"))
