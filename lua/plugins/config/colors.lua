71835e899491960c578fef1e275320789d9bdc97
Phandal <bfeld.111@gmail.com>
2024-04-03 10:54:07 -0500

    moved to github dark high contrast

--- lua/plugins/config/colors.lua
+++ lua/plugins/config/colors.lua
@@ -10,35 +10,35 @@
 -- require('nord').set()
 
 -- Gruvbox ColorScheme
-require("gruvbox").setup({
-  terminal_colors = true,  -- add neovim terminal colors
-  undercurl = false,
-  underline = true,
-  bold = true,
-  italic = {
-    strings = true,
-    emphasis = true,
-    comments = true,
-    operators = false,
-    folds = true,
-  },
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true,  -- invert background for search, diffs, statuslines and errors
-  contrast = "",   -- can be "hard", "soft" or empty string
-  palette_overrides = {},
-  overrides = {
-    LspReferenceRead = { bg = "#3c3836" },
-    LspReferenceText = { bg = "#3c3836" },
-    LspReferenceWrite = { bg = "#3c3836" },
-  },
-  dim_inactive = false,
-  transparent_mode = false,
-})
-vim.cmd("colorscheme gruvbox")
+-- require("gruvbox").setup({
+--   terminal_colors = true,  -- add neovim terminal colors
+--   undercurl = false,
+--   underline = true,
+--   bold = true,
+--   italic = {
+--     strings = true,
+--     emphasis = true,
+--     comments = true,
+--     operators = false,
+--     folds = true,
+--   },
+--   strikethrough = true,
+--   invert_selection = false,
+--   invert_signs = false,
+--   invert_tabline = false,
+--   invert_intend_guides = false,
+--   inverse = true,  -- invert background for search, diffs, statuslines and errors
+--   contrast = "",   -- can be "hard", "soft" or empty string
+--   palette_overrides = {},
+--   overrides = {
+--     LspReferenceRead = { bg = "#3c3836" },
+--     LspReferenceText = { bg = "#3c3836" },
+--     LspReferenceWrite = { bg = "#3c3836" },
+--   },
+--   dim_inactive = false,
+--   transparent_mode = false,
+-- })
+-- vim.cmd("colorscheme gruvbox")
 
--- Github Dark ColorScheme
--- vim.cmd("colorscheme github_dark_dimmed")
+-- Github Dark High Contranst ColorScheme
+vim.cmd("colorscheme github_dark_high_contrast")
