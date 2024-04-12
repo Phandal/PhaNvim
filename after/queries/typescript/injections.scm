ec5491f0a482bee4193cbd02f8b3afba30d3398e
Phandal <bfeld.111@gmail.com>
2024-04-12 12:06:44 -0500

    added treesitter injections for comments with spaces

--- after/queries/typescript/injections.scm
+++ after/queries/typescript/injections.scm
@@ -10,6 +10,15 @@
 
 (lexical_declaration
   (variable_declarator 
+    (comment) @comment (#eq? @comment "/* sql */")
+    (template_string) @injection.content (#set! injection.language "sql")))
+
+(return_statement
+  (comment) @comment (#eq? @comment "/* sql */")
+  (template_string) @injection.content (#set! injection.language "sql"))
+
+(lexical_declaration
+  (variable_declarator 
     (comment) @comment (#eq? @comment "/*html*/")
     (template_string) @injection.content (#set! injection.language "html")))
 
