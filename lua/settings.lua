a0203a035f1cce4aaeba7b1b9134c339c508e5fb
Phandal <bfeld.111@gmail.com>
2024-05-20 18:18:57 -0500

    added cursor vfx for neovide

--- lua/settings.lua
+++ lua/settings.lua
@@ -72,4 +72,5 @@ if vim.g.neovide then
   vim.g.neovide_scroll_animation_length = 0.1
   vim.g.neovide_confirm_quit = true
   vim.g.neovide_scale_factor = 0.9
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
 end
