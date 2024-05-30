56537b3c667d2acd28909f854a4eeb582c29e03e
Phandal <bfeld.111@gmail.com>
2024-05-29 20:13:58 -0500

    nord theme

--- lua/plugins/config/colors.lua
+++ lua/plugins/config/colors.lua
@@ -1,43 +1,43 @@
 -- Nord ColorScheme
--- vim.g.nord_contrast = true
--- vim.g.nord_borders = true
--- vim.g.nord_disable_background = false
--- vim.g.nord_cursorline_transparent = false
--- vim.g.nord_enable_sidebar_background = true
--- vim.g.nord_italic = true
--- vim.g.nord_uniform_diff_background = false
--- vim.g.nord_bold = true
--- require('nord').set()
+vim.g.nord_contrast = true
+vim.g.nord_borders = true
+vim.g.nord_disable_background = false
+vim.g.nord_cursorline_transparent = false
+vim.g.nord_enable_sidebar_background = true
+vim.g.nord_italic = true
+vim.g.nord_uniform_diff_background = false
+vim.g.nord_bold = true
+require('nord').set()
 
 -- Gruvbox ColorScheme
-require("gruvbox").setup({
-  terminal_colors = true, -- add neovim terminal colors
-  undercurl = false,
-  underline = true,
-  bold = true,
-  italic = {
-    strings = true,
-    emphasis = true,
-    comments = true,
-    operators = false,
-    folds = true,
-  },
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "",  -- can be "hard", "soft" or empty string
-  palette_overrides = {},
-  overrides = {
-    LspReferenceRead = { bg = "#3c3836" },
-    LspReferenceText = { bg = "#3c3836" },
-    LspReferenceWrite = { bg = "#3c3836" },
-  },
-  dim_inactive = false,
-  transparent_mode = false,
-})
+-- require("gruvbox").setup({
+--   terminal_colors = true, -- add neovim terminal colors
+--   undercurl = false,
+--   underline = true,
+--   bold = true,
+--   italic = {
+--     strings = true,
+--     emphasis = true,
+--     comments = true,
+--     operators = false,
+--     folds = true,
+--   },
+--   strikethrough = true,
+--   invert_selection = false,
+--   invert_signs = false,
+--   invert_tabline = false,
+--   invert_intend_guides = false,
+--   inverse = true, -- invert background for search, diffs, statuslines and errors
+--   contrast = "",  -- can be "hard", "soft" or empty string
+--   palette_overrides = {},
+--   overrides = {
+--     LspReferenceRead = { bg = "#3c3836" },
+--     LspReferenceText = { bg = "#3c3836" },
+--     LspReferenceWrite = { bg = "#3c3836" },
+--   },
+--   dim_inactive = false,
+--   transparent_mode = false,
+-- })
 
 -- Github Dark High Contranst ColorScheme
 -- require('github-theme').setup({
@@ -88,5 +88,5 @@ require("gruvbox").setup({
 if vim.g.neovide then
   vim.cmd("colorscheme github_dark_default")
 else
-  vim.cmd("colorscheme github_dark_high_contrast")
+  vim.cmd("colorscheme nord")
 end
--- lua/plugins/config/lsp.lua
+++ lua/plugins/config/lsp.lua
@@ -4,7 +4,7 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setqflist, opts)
 
 -- Setup capabilities for nvim-cmp
 -- local capabilities = vim.lsp.protocol.make_client_capabilities()
