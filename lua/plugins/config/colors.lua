4ca5c930b0d5358d2139702f0d678efbf12c64c3
Phandal <bfeld.111@gmail.com>
2024-07-02 22:52:01 -0500

    gruvbox theme again

--- lua/plugins/config/colors.lua
+++ lua/plugins/config/colors.lua
@@ -7,37 +7,37 @@ vim.g.nord_enable_sidebar_background = true
 vim.g.nord_italic = true
 vim.g.nord_uniform_diff_background = false
 vim.g.nord_bold = true
-require('nord').set()
+-- require('nord').set()
 
 -- Gruvbox ColorScheme
--- require("gruvbox").setup({
---   terminal_colors = true, -- add neovim terminal colors
---   undercurl = false,
---   underline = true,
---   bold = true,
---   italic = {
---     strings = true,
---     emphasis = true,
---     comments = true,
---     operators = false,
---     folds = true,
---   },
---   strikethrough = true,
---   invert_selection = false,
---   invert_signs = false,
---   invert_tabline = false,
---   invert_intend_guides = false,
---   inverse = true, -- invert background for search, diffs, statuslines and errors
---   contrast = "",  -- can be "hard", "soft" or empty string
---   palette_overrides = {},
---   overrides = {
---     LspReferenceRead = { bg = "#3c3836" },
---     LspReferenceText = { bg = "#3c3836" },
---     LspReferenceWrite = { bg = "#3c3836" },
---   },
---   dim_inactive = false,
---   transparent_mode = false,
--- })
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = false,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "",  -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {
+    LspReferenceRead = { bg = "#3c3836" },
+    LspReferenceText = { bg = "#3c3836" },
+    LspReferenceWrite = { bg = "#3c3836" },
+  },
+  dim_inactive = false,
+  transparent_mode = false,
+})
 
 -- Github Dark High Contranst ColorScheme
 -- require('github-theme').setup({
@@ -88,5 +88,5 @@ require('nord').set()
 if vim.g.neovide then
   vim.cmd("colorscheme github_dark_default")
 else
-  vim.cmd("colorscheme nord")
+  vim.cmd("colorscheme gruvbox")
 end
