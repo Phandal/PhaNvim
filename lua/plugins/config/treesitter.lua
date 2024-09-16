efa77615273b7b172fb32f6a18b0c4d6bacd7985
Phandal <bfeld.111@gmail.com>
2024-09-16 11:08:25 -0500

    added indent and incremental-selection modules in tree-sitter

--- lua/plugins/config/treesitter.lua
+++ lua/plugins/config/treesitter.lua
@@ -1,5 +1,11 @@
-require("nvim-treesitter.configs").setup{
+require("nvim-treesitter.configs").setup {
   highlight = {
+    enable = true
+  },
+  incremental_selection = {
+    enable = true
+  },
+  indent = {
     enable = true
   }
 }
